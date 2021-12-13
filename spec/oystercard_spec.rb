f1f0fd8246e39b4c5d55612ab21a18eefd6d78b9
Raven Sears <ravenrosereynolds@gmail.com>
2021-12-13 16:42:36 +0000

    add balance tests and code

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,12 @@ describe Oystercard do
     it 'Equals 0' do
       expect(subject.balance).to eq 0
     end
+
+    describe '#top_up' do
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+    
+      it 'can top up balance' do
+        expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      end
+    end
 end
-
-
